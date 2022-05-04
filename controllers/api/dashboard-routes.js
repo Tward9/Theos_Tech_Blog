7eb6857925d498ddcd85c95cd37f8bb00b2599ed
Theo Ward <theoward9@gmail.com>
2022-05-04 17:20:49 -0500

    create and delete posts from page

--- controllers/api/dashboard-routes.js
+++ controllers/api/dashboard-routes.js
@@ -23,4 +23,21 @@ router.post('/', async (req, res) => {
         res.status(400).json(err);
     }
 });
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const dbBlogPostData = await BlogPost.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        if (!dbBlogPostData) {
+            res.status(404).json({ message: 'No Post found with this id!' });
+            return;
+        };
+        res.status(200).json(dbBlogPostData);
+    } catch (err) {
+        res.status(500).json(err);
+    };
+});
 module.exports = router;
